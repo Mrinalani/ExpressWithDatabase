eda1231f8a51303c56a02085d6b4f936cf7e486f
pandeymrinalani <pandeymrinalani02@gmail.com>
2023-07-13 11:41:15 +0530

    savingtodatabase

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Vikas1998'
+});
+
+module.exports = pool.promise();
